96227fddefec2e95e87a312764192c1e5931dd49
Radovan <studio@pythonforce.com>
2018-12-30 15:33:10 -0500

    modelform working

--- profiles/core/views.py
+++ profiles/core/views.py
@@ -1,3 +1,6 @@
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Profile
 from .forms import ProfileForm
@@ -17,4 +20,11 @@ def profile(request, profile_slug):
 def edit_profile(request, profile_slug):
     profile = get_object_or_404(Profile, slug=profile_slug)
     form = ProfileForm(instance=profile)
+
+    if request.method == "POST":
+        form = ProfileForm(instance=profile, data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "{} {} profile updated.".format(form.cleaned_data["first_name"], form.cleaned_data["last_name"]))
+            return HttpResponseRedirect(profile.get_absolute_url())
     return render(request, "edit-profile.html", {'form':form})
