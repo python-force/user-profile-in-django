16eb4704b90d56d16a06e6a3f0d46f8077020c52
Radovan <studio@pythonforce.com>
2018-12-30 14:08:58 -0500

    profile model

--- profiles/core/models.py
+++ profiles/core/models.py
@@ -1,36 +1,17 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # import pytz
 # from django.utils.text import slugify
 
-
-class Test(models.Model):
-    title = models.CharField(max_length=255, blank=True, null=True)
-
-
-class Mineral(models.Model):
-    """Mineral Model"""
-    pub_date = models.DateTimeField(default=timezone.now)
-    name = models.CharField(max_length=500, blank=True, null=True)
-    image_filename = models.CharField(max_length=500, blank=True, null=True)
-    image_caption = models.CharField(max_length=500, blank=True, null=True)
-    category = models.CharField(max_length=500, blank=True, null=True)
-    formula = models.CharField(max_length=500, blank=True, null=True)
-    strunz_classification = models.CharField(max_length=500, blank=True, null=True)
-    color = models.CharField(max_length=500, blank=True, null=True)
-    crystal_system = models.CharField(max_length=500, blank=True, null=True)
-    unit_cell = models.CharField(max_length=500, blank=True, null=True)
-    crystal_symmetry = models.CharField(max_length=500, blank=True, null=True)
-    cleavage = models.CharField(max_length=500, blank=True, null=True)
-    mohs_scale_hardness = models.CharField(max_length=500, blank=True, null=True)
-    luster = models.CharField(max_length=500, blank=True, null=True)
-    streak = models.CharField(max_length=500, blank=True, null=True)
-    diaphaneity = models.CharField(max_length=500, blank=True, null=True)
-    optical_properties = models.CharField(max_length=500, blank=True, null=True)
-    refractive_index = models.CharField(max_length=500, blank=True, null=True)
-    crystal_habit = models.CharField(max_length=500, blank=True, null=True)
-    specific_gravity = models.CharField(max_length=500, blank=True, null=True)
-    group = models.CharField(max_length=500, blank=True, null=True)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(max_length=100, blank=True)
+    date_of_birth = models.DateField(max_length=12, blank=True)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(blank=True)
 
     def __str__(self):
-        return self.name
+        return "{} {}".format(self.first_name, self.last_name)
