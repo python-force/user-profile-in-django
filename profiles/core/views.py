c5b633553c2648827a6e8d57dfc7332809e90378
Radovan <studio@pythonforce.com>
2019-01-01 17:04:01 -0500

    reverse

--- profiles/core/views.py
+++ profiles/core/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 from django.contrib.auth import update_session_auth_hash
 from django.contrib import messages
 from django.http import HttpResponseRedirect
@@ -45,7 +46,7 @@ def change_password(request, profile_slug):
             user = form.save()
             update_session_auth_hash(request, user)  # Important!
             messages.success(request, 'Your password was successfully updated!')
-            return HttpResponseRedirect('index')
+            return HttpResponseRedirect(reverse('index'))
         else:
             messages.error(request, 'Please correct the error below.')
     else:
