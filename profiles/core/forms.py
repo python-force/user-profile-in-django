d7c1a08e96079636d712a53b28798a271e00a540
Radovan <studio@pythonforce.com>
2019-01-01 16:04:53 -0500

    adding import

--- profiles/core/forms.py
+++ profiles/core/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.hashers import check_password
 from bootstrap_datepicker_plus import DatePickerInput
+from zxcvbn_password import zxcvbn
 from zxcvbn_password.fields import PasswordField, PasswordConfirmationField
 from markdownx.fields import MarkdownxFormField
 from django import forms
