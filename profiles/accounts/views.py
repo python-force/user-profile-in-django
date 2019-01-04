ff23625f6b3dd9c44d5f1318150e8aa22501e23b
Radovan <studio@pythonforce.com>
2019-01-04 18:36:05 -0500

    signup form sinal

--- profiles/accounts/views.py
+++ profiles/accounts/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import SignUpForm
+from django.contrib.auth import login, authenticate
 
 
 def sign_in(request):
@@ -32,6 +34,48 @@ def sign_in(request):
 
 
 def sign_up(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()  # load the profile instance created by the signal
+            user.profile.first_name = form.cleaned_data.get('first_name')
+            user.profile.last_name = form.cleaned_data.get('last_name')
+            user.save()
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1']
+            )
+            login(request, user)
+            messages.success(
+                request,
+                "You're now a user! You've been signed in, too."
+            )
+            return HttpResponseRedirect(reverse('index'))  # TODO: go to profile
+    return render(request, 'accounts/sign_up.html', {'form': form})
+
+"""
+def sign_up(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()  # load the profile instance created by the signal
+            user.profile.first_name = form.cleaned_data.get('first_name')
+            user.profile.last_name = form.cleaned_data.get('last_name')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return HttpResponseRedirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/sign_up.html', {'form': form})
+"""
+
+"""
+def sign_up(request):
     form = UserCreationForm()
     if request.method == 'POST':
         form = UserCreationForm(data=request.POST)
@@ -48,6 +92,7 @@ def sign_up(request):
             )
             return HttpResponseRedirect(reverse('index'))  # TODO: go to profile
     return render(request, 'accounts/sign_up.html', {'form': form})
+"""
 
 
 def sign_out(request):
--- profiles/core/models.py
+++ profiles/core/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from django.utils.safestring import mark_safe
 from django.utils.text import slugify
 from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -12,9 +14,9 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    email_verify = models.EmailField(max_length=100)
-    date_of_birth = models.DateField(max_length=12)
+    email = models.EmailField(max_length=100, blank=True, null=True)
+    email_verify = models.EmailField(max_length=100, blank=True, null=True)
+    date_of_birth = models.DateField(max_length=12, blank=True, null=True)
     bio = models.TextField(blank=True)
     avatar = models.ImageField(blank=True, upload_to='avatar')
     city = models.CharField(max_length=255, blank=True)
@@ -25,9 +27,10 @@ class Profile(models.Model):
     slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
-        self.slug = slugify("{}-{}-{}".format(self.first_name.lower(),
-                                              self.last_name.lower(), self.id))
-        super().save(*args, **kwargs)
+        if self.first_name or self.last_name:
+            self.slug = slugify("{}-{}-{}".format(self.first_name.lower(),
+                                                  self.last_name.lower(), self.id))
+            super().save(*args, **kwargs)
 
     def __str__(self):
         return "{} {}".format(self.first_name, self.last_name)
@@ -37,3 +40,10 @@ class Profile(models.Model):
 
     def display_bio(self):
         return mark_safe(self.bio)
+
+    @receiver(post_save, sender=User, dispatch_uid='save_new_user_profile')
+    def update_user_profile(sender, instance, created, **kwargs):
+        user = instance
+        if created:
+            profile = Profile(user=user)
+            profile.save()
