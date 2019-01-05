a0d7915e906bb70154b52dc597eda8cbbaaec163
Radovan <studio@pythonforce.com>
2019-01-04 21:46:08 -0500

    fixing slug and signals

--- profiles/core/models.py
+++ profiles/core/models.py
@@ -4,14 +4,15 @@ from django.contrib.auth.models import User
 from django.utils.safestring import mark_safe
 from django.utils.text import slugify
 from django.utils import timezone
+import random
 
 
 class Profile(models.Model):
     """Profile Model"""
     pub_date = models.DateTimeField(default=timezone.now)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=50, blank=True, null=True)
-    last_name = models.CharField(max_length=100, blank=True, null=True)
+    first_name = models.CharField(default='', max_length=50, blank=True)
+    last_name = models.CharField(default='', max_length=100, blank=True)
     email = models.EmailField(max_length=100, blank=True, null=True)
     email_verify = models.EmailField(max_length=100, blank=True, null=True)
     date_of_birth = models.DateField(max_length=12, blank=True, null=True)
@@ -25,10 +26,14 @@ class Profile(models.Model):
     slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
         if self.first_name or self.last_name:
             self.slug = slugify("{}-{}-{}".format(self.first_name.lower(),
                                                   self.last_name.lower(), self.id))
-            super().save(*args, **kwargs)
+        else:
+            self.slug = slugify('profile {}'.format(self.id))
+        super().save()
+
 
     def __str__(self):
         return "{} {}".format(self.first_name, self.last_name)
--- profiles/core/signals.py
+++ profiles/core/signals.py
@@ -2,11 +2,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from profiles.core.models import Profile
 from django.contrib.auth.models import User
-import pdb
 
 @receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
-        pdb.set_trace()
-    instance.profile.save()
+        profile = Profile.objects.create(user=instance)
